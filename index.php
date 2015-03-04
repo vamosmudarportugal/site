4a39eb232594d16806cf7990d944efef1e72a51f
vamosmudarportugal <vamosmudarportugal@dsantos.us>
2015-03-04 02:56:34 +0000

    Update index.php
    Mais lixo, mas preciso de ir dormir... :/

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
 Será quem um like chega para mudar o país? O teu.<br>
 já agora... amanhã queria aqui meter como se muda portugal, tens alguma ideia?<br>
 Então passa cá amanhã lá pela noite dentro e deixa cá a tua.<br>
+
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'vamosmudarportugal';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
