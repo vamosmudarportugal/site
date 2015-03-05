3f3a826d478e9501276df58fb4e97adab56372d2
vamosmudarportugal <vamosmudarportugal@dsantos.us>
2015-03-05 20:47:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
 
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Vamos mudar portugal</title>
+        <title>vamosmudarportugal.org</title>
         <meta name="description" content="">
         <meta name="author" content="ink, cookbook, recipes">
         <meta name="HandheldFriendly" content="True">
@@ -27,13 +27,11 @@
         <!-- test browser flexbox support and load legacy grid if unsupported -->
         <script type="text/javascript" src="http://cdn.ink.sapo.pt/3.1.1/js/modernizr.js"></script>
         <script type="text/javascript">
-            //if (window.location.protocol != "https:")
-            //    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
             Modernizr.load({
               test: Modernizr.flexbox,
               nope : 'http://cdn.ink.sapo.pt/3.1.1/css/ink-legacy.min.css'
             });
-                    </script>
+        </script>
 
         <!-- load Ink's javascript files from the cdn -->
         <script type="text/javascript" src="http://cdn.ink.sapo.pt/3.1.1/js/holder.js"></script>
@@ -53,97 +51,81 @@
                 font-size: 1.6em;
             }
 
-            article header{
-                padding: 0;
-                overflow: hidden;
-            }
-
-            article footer {
-                background: none;
-            }
-
-            article {
-                padding-bottom: 2em;
-                border-bottom: 1px solid #ccc;
-            }
-
-            .date {
-                float: right;
-            }
-
-            summary {
-                font-weight: 700;
-                line-height: 1.5;
-            }
-
             footer {
                 background: #ccc;
             }
+
         </style>
+
     </head>
 
     <body>
-
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=645938285549890&version=v2.0";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        </script>
         <div class="ink-grid">
 
+            <!--[if lte IE 9 ]>
+            <div class="ink-alert basic" role="alert">
+                <button class="ink-dismiss">&times;</button>
+                <p>
+                    <strong>You are using an outdated Internet Explorer version.</strong>
+                    Please <a href="http://browsehappy.com/">upgrade to a modern browser</a> to improve your web experience.
+                </p>
+            </div>
+            -->
 
             <!-- Add your site or application content here -->
 
-            <header class="clearfix vertical-padding">
+            <header class="vertical-space">
+                <h1>Vamos mudar Portugal<small>vamos todos discutir como?</small></h1>
+                <div class="fb-like" data-href="http://vamosmudarportugal.org" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                <nav class="ink-navigation">
+                    <ul class="menu horizontal black">
+                        <li class="active"><a href="https://vamosmudarportugal.org/#comment-1889946659">Motivações</a></li>
 
-                <h1 class="logo xlarge-push-left large-push-left">
-                    Vamos mudar Portugal<small>e podes ajudar a discutir como?</small>
-                </h1>
-
+                    </ul>
+                </nav>
             </header>
-
-            <section class="column-group gutters article push-left">
-                <div class="xlarge-70 large-70 medium-60 small-100 tiny-100">
-                    <article>
-                        <header>
-                            <h1 class="push-left">Como discutir?</h1>
-                        </header>
-                        <summary>O que começa com uma revolta por situações alheias, é agora a minha causa.</summary>
-                        <div id="disqus_thread"></div>
-                            <script type="text/javascript">
-                                /* * * CONFIGURATION VARIABLES * * */
-                                var disqus_shortname = 'vamosmudarportugal';
-
-                                /* * * DON'T EDIT BELOW THIS LINE * * */
-                                (function() {
-                                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-                                })();
-                            </script>
-                            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
-
-                    </article>
+            <div class="column-group gutters">
+                <div class="all-100">
+                    <h2>Discuções em curso</h2>
                 </div>
-            </section>
-            <section class="xlarge-30 large-30 medium-40 small-100 tiny-100 push-right">
-                <h2>Projectos Amigos</h2>
-                <ul class="unstyled">
-                    <li class="column-group half-gutters">
-                        <div class="all-60 small-50 tiny-50"></div>
-                    </li>
-
-                </ul>
-            </section>
+            </div>
+            <div class="column-group gutters">
+                <div id="disqus_thread"></div>
+                    <script type="text/javascript">
+                        /* * * CONFIGURATION VARIABLES * * */
+                        var disqus_shortname = 'vamosmudarportugal';
+                        /* * * DON'T EDIT BELOW THIS LINE * * */
+                        (function() {
+                            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                        })();
+                    </script>
+                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            </div>
         </div>
         <footer class="clearfix">
             <div class="ink-grid">
                 <ul class="unstyled inline half-vertical-space">
                     <li class="active"><a href="https://vamosmudarportugal.org/#comment-1889946659">About</a></li>
                 </ul>
-                <p class="note">Vamosmudarportugal.org o site que tu podes fazer mudar. :)</p>
+                <p class="note">vamosmudarportugal.org 2015.</p>
             </div>
         </footer>
     </body>
-    <!--
+</html>
+<!--
 Em nome da transparência! ;)
 <?php
 echo file_get_contents('/home/vamosmudarportugal_org/public_html/git.txt');
 ?>
 -->
-</html>
